f3bb7b5fabdbdb99a1d1488c3a3496b167a8619f
Surya Santosh Mani null <surya.talluri@wavemaker.com>
2016-01-19 07:30:56 +0000

    New files added - [rechtspraak_apiTarget.json,Calculator_API.json,Test13uu798ProcedureExecutorService.java,Table1Service.java,FeedService_API.json,CalculatorController.java,Test13uu798-queries.hbm.xml,Table1Controller.java,Test13uu798_API.json,googleapis_apiTarget.json,randomuser_apiTarget.json,Test13uu798QueryExecutorService.java,randomuser_connection_settings.json,CalculatorService.java,servicedef.xml,Test13uu798ProcedureExecutorServiceImpl.java,Table1.java,Test13uu798_vk.hbm.xml,MultiplyResponse.java,rechtspraak_apiTarget.json,Calculator.settings.json,Test13uu798_query.json,googleapis_API.json,servicedef.xml,randomuser.spring.xml,rechtspraak_connection_settings.json,Multiply.java,Calculator.spring.xml,servicedef.xml,Calculator.wsdl,googleapis_connection_settings.json,CalculatorSoap_CalculatorSoap_Client.java,servicedef.xml,randomuser_apiTarget.json,db-connection-settings.json,Add.java,Test13uu798.spring.xml,QueryExecutionController.java,ObjectFactory.java,googleapis.spring.xml,Table1ServiceImpl.java,Subtract.java,CalculatorSoap_CalculatorSoap12_Client.java,rechtspraak.spring.xml,googleapis_apiTarget.json,Test13uu798QueryExecutorServiceImpl.java,Table1Dao.java,ProcedureExecutionController.java,Test13uu798_procedure.json,package-info.java,rechtspraak_API.json,Test13uu798.properties,servicedef.xml,Divide.java,randomuser_API.json,AddResponse.java,CalculatorSoap.java,Calculator.java,Test13uu798_published_dataModel.json,SubtractResponse.java,Test13uu798-procedures.mappings.json,FeedService.spring.xml,DivideResponse.java,servicedef.xml] Files modified - [page.min.html,Main.html,footer.variables.json,page.min.html,rightnav.variables.json,project-services.xml,Common.variables.json,page.min.html,page.min.html,page.min.html,Login.variables.json,wmProperties.js,app.variables.json,topnav.variables.json,Main.variables.json,leftnav.variables.json,production.properties,header.variables.json,Main.js,page.min.html,page.min.html,page.min.html,development.properties] Refactored files - [] Fixed bugs - [] Removed files - []

--- src/main/webapp/pages/Main/Main.js
+++ src/main/webapp/pages/Main/Main.js
@@ -1,21 +1,49 @@
 Application.$controller("MainPageController", ["$scope", function ($scope) {
     "use strict";
 
-    /* perform any action on the variables within this block(on-page-load) */
+    /* perform any action with the variables inside this block(on-page-load) */
     $scope.onPageVariablesReady = function () {
         /*
          * variables can be accessed through '$scope.Variables' property here
-         * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
-         * $scope.Variables.loggedInUser.getData()
+         * e.g. $scope.Variables.staticVariable1.getData()
          */
     };
-    /* perform any action on widgets within this block */
+    
+    /* perform any action with widgets inside this block */
     $scope.onPageReady = function () {
         /*
          * widgets can be accessed through '$scope.Widgets' property here
-         * e.g. to get value of text widget named 'username' use following script
-         * '$scope.Widgets.username.datavalue'
+         * e.g. $scope.Widgets.byId(), $scope.Widgets.byName()or access widgets by $scope.Widgets.widgetName
          */
     };
 
 }]);
+
+
+Application.$controller("Table1Grid1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
+
+Application.$controller("Table1Liveform1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
+
+Application.$controller("Table1Grid2Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
+
+Application.$controller("Table1Liveform2Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
